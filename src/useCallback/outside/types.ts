0b5d96284152e316d9391e9b2c9171b1c05ee1f9
fipnooone <fipnone@gmail.com>
2023-01-17 10:29:13 +0700

    useoutside type fix

--- src/useCallback/outside/types.ts
+++ src/useCallback/outside/types.ts
@@ -4,7 +4,7 @@ type Callback = () => void;
 
 interface UseOutside {
     <T extends HTMLElement>(ref: RefObject<T>, callback: Callback, deps?: DependencyList): void;
-    <T extends HTMLElement[]>(refs: RefObject<T>[], callback: Callback, deps?: DependencyList): void;
+    <T extends HTMLElement>(refs: RefObject<T>[], callback: Callback, deps?: DependencyList): void;
 }
 
 export type { Callback, UseOutside };
--- types/useCallback/outside/types.d.ts
+++ types/useCallback/outside/types.d.ts
@@ -2,6 +2,6 @@ import { DependencyList, RefObject } from 'react';
 type Callback = () => void;
 interface UseOutside {
     <T extends HTMLElement>(ref: RefObject<T>, callback: Callback, deps?: DependencyList): void;
-    <T extends HTMLElement[]>(refs: RefObject<T>[], callback: Callback, deps?: DependencyList): void;
+    <T extends HTMLElement>(refs: RefObject<T>[], callback: Callback, deps?: DependencyList): void;
 }
 export type { Callback, UseOutside };
